e098efd388373ecd1e118383549e7e1312855369
PRANAV <pranavprasanth907@gmail.com>
2022-12-08 09:19:42 +0530

    netflix clone-main banneer movie changing on refresh,scroll,trailer video,hover effect,different genre,http requests through axios

---
+++ src/Constants/Constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_key = '16310cdb8d527b0c2c627888cd6ed437';
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
---
+++ src/urls.js
@@ -0,0 +1,7 @@
+import {API_key} from './Constants/Constants'
+// export const originals = `discover/tv?api_key=${API_key}&with_networks=213`
+export const originals = `discover/tv?api_key=${API_key}&with_networks=213`
+export const action = `discover/movie?api_key=${API_key}&with_genres=28`
+export const comedy = `discover/movie?api_key=${API_key}&with_genres=35`
+export const horror = `discover/movie?api_key=${API_key}&with_genres=27`
+export const documentaries = `discover/movie?api_key=${API_key}&with_genres=99`
